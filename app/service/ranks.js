dccf938e49189a16c60419a3a14e7199ae63cb5f
kunpeng.xkp <kunpeng.xkp@alibaba-inc.com>
2018-04-26 22:35:19 +0800

    使用本地文件数据库存储小时数据

--- app/service/ranks.js
+++ app/service/ranks.js
@@ -1,11 +1,29 @@
-// app/service/news.js
 const Service = require('egg').Service;
+var fs = require('fs');
+const low = require('lowdb');
+const moment = require('moment');
+const FileSync = require('lowdb/adapters/FileSync');
 
 class RanksService extends Service {
   async list(keyword = '', sort = 0, page = 1) {
-    // read config
+    if (!fs.existsSync('./data/')) {
+      fs.mkdirSync('./data/');
+    }
+    if (!fs.existsSync('./data/db.json')) {
+      fs.writeFileSync('./data/db.json', '');
+    }
+    const adapter = new FileSync('./data/db.json');
+    const db = low(adapter);
+    db.defaults({ rankItem: [] }).write();
+    const id = moment().format('YYYYMMDDHH');
+    const where = { id, keyword, sort, page };
+    let result = db.get('rankItem').filter(where).value();
+    if (result && result.length > 0) {
+      console.log('get db...', where);
+      return result;
+    }
+    console.log('get jd api...', where);
     const { serverUrl } = this.config.jd;
-
     const { data } = await this.ctx.curl(`${serverUrl}/ware/searchList.action`, {
       method: 'POST',
       data: {
@@ -16,13 +34,15 @@ class RanksService extends Service {
       },
       dataType: 'json',
     });
-
     const list = this.parseList(data);
-    const result = [];
+    const tmpResult = [];
     list.map((item) => {
-      result.push({ skuId: item.wareId, title: item.wname, totalCount: item.totalCount, imageUrl: item.imageurl, price: item.jdPrice });
+      const a = { skuId: item.wareId, title: item.wname, totalCount: item.totalCount, imageUrl: item.imageurl, price: item.jdPrice };
+      const b = Object.assign({}, a, where);
+      tmpResult.push(a);
+      db.get('rankItem').push(b).write();
     });
-
+    result = tmpResult;
     return result;
   }
 
