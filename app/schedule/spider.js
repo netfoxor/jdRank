a670c710481a39db6d87bc564a9a24c2b89b50be
kunpeng.xkp <kunpeng.xkp@alibaba-inc.com>
2018-05-03 20:23:48 +0800

    调整关键词

--- app/schedule/spider.js
+++ app/schedule/spider.js
@@ -13,11 +13,27 @@ class Spider extends Subscription {
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
     const now = new Date();
-    const keywords = ['女童连衣裙', '女童连衣裙夏', '女童连衣裙夏装', '女童裙子', '儿童连衣裙', '儿童裙子', '亲子装', '亲子套装', '全家装', '亲子装全家装', '亲子装全家装 夏装'];
+    const keywords = [
+      '女童连衣裙',
+      '儿童连衣裙',
+      '女童公主裙',
+      '公主裙女童',
+      '儿童裙子',
+      '女大童连衣裙',
+      '儿童夏装女',
+      '童装女夏装',
+      '女童连衣裙夏',
+
+      '亲子装',
+      '亲子套装',
+      '亲子装夏装',
+
+      '女童套装',
+    ];
     for (let i = 0; i < keywords.length; i++) {
       const keyword = keywords[i];
-      await this.ctx.service.ranks.list(now, keyword, 0, 1000, true);
-      await this.ctx.service.ranks.list(now, keyword, 1, 1000, true);
+      await this.ctx.service.ranks.list(now, keyword, 0, 500, true);
+      await this.ctx.service.ranks.list(now, keyword, 1, 500, true);
     }
   }
 }
